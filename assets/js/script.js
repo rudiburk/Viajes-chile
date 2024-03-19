3c2770caf025f207c75f8bd789fd4976330b1e2f
rudiburk <rudi.burk.r@gmail.com>
2024-03-19 01:50:30 -0300

    commit final, trabajo terminado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+
 $(document).ready(function(){
     $('[data-toggle="tooltip"]'). tooltip();
+        });
+
+document.addEventListener("DOMContentLoaded", function() {
+        document.querySelectorAll('.nav-link').forEach(anchor => {
+            anchor.addEventListener('click', function(e) {
+                e.preventDefault();
+                const targetId = this.getAttribute('href').substring(1);
+                const targetSection = document.getElementById(targetId);
+                if (targetSection) {
+                    targetSection.scrollIntoView({
+                        behavior: 'smooth'
+                    });
+                }
+            });
+        });
     });
